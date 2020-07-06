87f4413e0412a841ba81149b2d6c820ef4e1f62a
Automatic Build <mauro.masucci@leidodscloud.onmicrosoft.com>
2020-07-06 16:42:52 +0100

    solution init

---
+++ HIE_CDS/WebResources/leidos_projectAmendment.js
@@ -0,0 +1,11 @@
+var Leidos = window.Leidos || {};
+
+Leidos.SetProjectAmendmentPathControlViews = function (executionContext) {
+   "use strict";
+    Leidos.setLookupViewId(executionContext, "header_process_leidos_gradeestaff","{aa3f26b8-b6bb-ea11-a812-000d3a7f1bbb}");
+    Leidos.setLookupViewId(executionContext, "header_process_leidos_gradefstaff","{da8a1bc4-b6bb-ea11-a812-000d3a7f1bbb}");
+    Leidos.setLookupViewId(executionContext, "header_process_leidos_executivegradestaff","{AB53A03A-B7BB-EA11-A812-000D3A7FEDBE}");
+    Leidos.setLookupViewId(executionContext, "header_process_leidos_leadershipteam","{21C34315-32BC-EA11-A812-000D3A7FEDBE}");
+    Leidos.setLookupViewId(executionContext, "header_process_leidos_hiechiefexecutive","{78B7A846-B7BB-EA11-A812-000D3A7FEDBE}");
+    Leidos.setLookupViewId(executionContext, "header_process_leidos_hieboard","{1359B25E-B7BB-EA11-A812-000D3A7FEDBE}");
+}
