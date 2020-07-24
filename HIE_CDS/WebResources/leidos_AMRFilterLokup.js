2c5c80ddddcca9a8458274b96e4cc8c17086f00a
Automatic Build <mauro.masucci@leidodscloud.onmicrosoft.com>
2020-07-24 11:22:09 +0100

    solution init

--- HIE_CDS/WebResources/leidos_AMRFilterLokup.js
+++ HIE_CDS/WebResources/leidos_AMRFilterLokup.js
@@ -1,72 +1,39 @@
+var Leidos = window.Leidos || {};
 
- var Leidos = window.Leidos || {};
+Leidos.filterSegmentationLookup = function(executionContext) {
+  "use strict";
+  var formContext = executionContext.getFormContext();
+  //Check if the control exist on the form
+  if (formContext.getControl("header_process_leidos_proposedsegmentation") !== null) {
+    // add the event handler for PreSearch Event
+    formContext.getControl("header_process_leidos_proposedsegmentation").addPreSearch(Leidos.addFilter);
+  }
+}
 
-
- Leidos.filterSegmentationLookup = function(executionContext) {
-
-    var formContext = executionContext.getFormContext();
-
-//Check if the control exist on the form
-
-            if (formContext.getControl("header_process_leidos_proposedsegmentation") != null) {
-
-            // add the event handler for PreSearch Event
-
-            formContext.getControl("header_process_leidos_proposedsegmentation").addPreSearch(Leidos.addFilter);
-
+Leidos.addFilter = function(executionContext) {
+  "use strict";
+  var formContext = executionContext.getFormContext();
+  var parentId = null;
+  var parentlookup;
+  var fetchQuery;
+  try {
+    //Check if control exist on form
+    if (formContext.getControl("leidos_businessorsocialenterprise") !== null && formContext.getControl("leidos_businessorsocialenterprise").getAttribute().getValue() !== null) {
+      //Get Account lookup value
+      parentlookup = formContext.getControl("leidos_businessorsocialenterprise").getAttribute().getValue();
+      //Get the account id
+      parentId = parentlookup[0].id;
     }
-
+    //Build fetch
+    if (parentId !== null || parentId !== undefined) {
+      fetchQuery = "<filter type='and'>" +
+        "<condition attribute='statecode' operator='eq' value='0' />" +
+        "<condition attribute='leidos_businessorsocialenterprise' operator='eq' value='" + parentId + "' />" +
+        "</filter>";
+      //add custom filter
+      formContext.getControl("header_process_leidos_proposedsegmentation").addCustomFilter(fetchQuery);
+    }
+  } catch (e) {
+    Xrm.Navigation.openAlertDialog("addFilter Error: " + (e.description || e.message));
+  }
 }
-
- Leidos.addFilter = function(executionContext) {
-
-    var formContext = executionContext.getFormContext();   
-
-        var parentId= null;
-
-        var parentlookup;
-
-        var fetchQuery;
-
-        try {
-
-                //Check if control exist on form
-
-                if (formContext.getControl("leidos_businessorsocialenterprise") != null && formContext.getControl("leidos_businessorsocialenterprise").getAttribute().getValue() != null) {
-
-                //Get Account lookup value
-
-                parentlookup = formContext.getControl("leidos_businessorsocialenterprise").getAttribute().getValue();
-
-                //Get the account id
-
-                parentId= parentlookup[0].id;
-
-                }
-
-                //Build fetch
-
-                if (parentId!= null || parentId!= undefined) {
-
-                fetchQuery = "<filter type='and'>" +
-
-                "<condition attribute='statecode' operator='eq' value='0' />" +
-
-                "<condition attribute='leidos_businessorsocialenterprise' operator='eq' value='" + parentId+ "' />" +
-
-                "</filter>";
-
-                //add custom filter
-
-                formContext.getControl("header_process_leidos_proposedsegmentation").addCustomFilter(fetchQuery);
-
-                }
-
-        } catch (e) {
-
-        Xrm.Utility.alertDialog("addFilter Error: " + (e.description || e.message));
-
-}
-
-}
-
